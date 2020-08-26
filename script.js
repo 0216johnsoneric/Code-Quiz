ae748603628e00743cefaeb5aede68cc376ce660
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-26 00:05:05 -0400

    Done.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// const seconds = document.querySelector("seconds");
 const startButton = document.getElementById("start");
 const nextButton = document.getElementById("next")
 const startText = document.getElementById("card-text");
 const questionList = document.getElementById("question-container")
 const questionElement = document.getElementById("question")
 const answerButtons = document.getElementById('answer-buttons')
-var secondsElement = document.getElementById("seconds")
+var secondsElement = document.getElementById("time-remaining")
 var messageElement = document.querySelector("h2");
 var choicesList = document.getElementById("choices");
 var formButton = document.createElement("button");
 var formElement = document.createElement("div");
 var mainElement = document.getElementById("main-content");
+var indicatorElement = document.getElementById("indicator");
 
 // var choicesList = document.querySelector("ul#choices");
 // var indicator = document.querySelector("div#indicator");
@@ -64,8 +64,8 @@ var questions = [
 init();
 
 function init() {
-    secondsLeft = 60;
     score = 0;
+    secondsLeft = 60;
 }
 
 function startGame() {
@@ -91,20 +91,55 @@ function newQuestions(questionNumber) {
     var questionItem = questions[questionNumber];
     messageElement.textContent = questionItem.question;
 
-    var newChoices = document.createElement("li");
-    var newChoices = document.createElements("button");
+    var newChoices = document.createElement("div");
+    // var newChoices = document.createElements("button");
     choicesList.appendChild(newChoices);
 
     for (var i = 0; i < questionItem.choices.length; i++) {
         var choice = questionItem.choices[i];
 
-        var li = document.createElement("button");
+        var li = document.createElement("li");
         li.setAttribute("data-index", i);
         li.textContent = choice;
         newChoices.appendChild(li);
+
+        li.addEventListener("click", function (event) {
+            if (
+                questionItem.answer ===
+                parseInt(event.target.getAttribute("data-index"))
+            ) {
+                score += 10;
+                indicatorElement.innerHTML = "<hr> Correct!";
+                indicatorElement.setAttribute("style", "color: brightgreen");
+            } else {
+                secondsLeft -= 10;
+                indicatorElement.innerHTML = "<hr> Try Again!";
+                indicatorElement.setAttribute("style", "color: red");
+            }
+
+            questionNumber++;
+
+            if (questionNumber === questions.length) {
+                clearInterval(timerInterval);
+                indicatorElement.textContent = "";
+                newChoices.remove();
+                messageElement.textContent = "Thanks for playing!";
+                messageElement.appendChild(startText);
+                startText.textContent = "Your final score is: " + score;
+
+                renderForm();
+            } else {
+                setTimeout(function () {
+                    newQuestions(questionNumber);
+                    newChoices.remove();
+                    indicatorElement.textContent = "";
+                }, 1000);
+            }
+        });
     }
-    
 }
 
 
+
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", newQuestions);
